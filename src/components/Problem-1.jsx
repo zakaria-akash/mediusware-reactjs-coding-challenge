55150b23ba67fc1e9018160b0cac50f13b7d840d
zakaria-akash <zakaria.93@yahoo.com>
2023-02-26 03:01:54 +0600

    second commit after finishing the first task (Problem-1)

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,137 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Problem1 = () => {
+  const [show, setShow] = useState("all");
+  const [inputDataActive, setInputDataActive] = useState([]);
+  const [inputDataCompleted, setInputDataCompleted] = useState([]);
+  const [inputDataOthers, setInputDataOthers] = useState([]);
+  const [dataToShow, setDataToShow] = useState([]);
 
-    const [show, setShow] = useState('all');
+  const handleClickAll = () => {
+    setShow("all");
 
-    const handleClick = (val) =>{
-        setShow(val);
+    setDataToShow(inputDataActive.concat(inputDataCompleted, inputDataOthers));
+  };
+
+  const handleClickActive = () => {
+    setShow("active");
+
+    setDataToShow(inputDataActive);
+  };
+
+  const handleClickCompleted = () => {
+    setShow("completed");
+
+    setDataToShow(inputDataCompleted);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const status = e.target.status.value;
+
+    const userData = {
+      id: Date.now().toString(36) + Math.random().toString(16).slice(2),
+      name: name,
+      status: status,
+    };
+
+    if (userData.status === "active") {
+      setInputDataActive((prevData) => [...prevData, userData]);
+    } else if (userData.status === "completed") {
+      setInputDataCompleted((prevData) => [...prevData, userData]);
+    } else {
+      setInputDataOthers((prevData) => [...prevData, userData]);
     }
 
-    return (
+    e.target.reset();
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
             </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                name="status"
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={handleClickAll}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={handleClickActive}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={handleClickCompleted}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataToShow.map((data) => {
+                return (
+                  <tr key={data.id}>
+                    <th scope="col">{data.name}</th>
+                    <th scope="col">{data.status}</th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
